d8db694ccd8ab741e51628a4a73ca887940c35df
Diego Moreno <iam@diegomoreno.co>
2020-03-28 22:49:06 -0500

    LinearLayout in Login Page

---
+++ app/src/main/java/co/diegomoreno/platzimusic/MainActivity.kt
@@ -0,0 +1,15 @@
+package co.diegomoreno.platzimusic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_profile)
+
+
+    }
+}
